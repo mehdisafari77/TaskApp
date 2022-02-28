7fb075e9e627aea11169c3d22998366b02dd74ce
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-02-28 14:11:33 -0800

    added and edited task Controller

---
+++ TaskApp/Model Controller/TaskController.swift
@@ -0,0 +1,43 @@
+//
+//  TaskController.swift
+//  TaskApp
+//
+//  Created by Mehdi MMS on 28/02/2022.
+//
+
+import CoreData
+
+class TaskController {
+    
+    static let shared = TaskController()
+    
+    var tasks: [Task] = []
+    
+    private lazy var fetchRequest: NSFetchRequest<Task> = {
+        let request = NSFetchRequest<Task>(entityName: "Task")
+        request.predicate = NSPredicate(value: true)
+        return request
+    }()
+    
+    func createTaskWith(name: String, notes: String?, dueDate: Date?) {
+        Task(name: name, notes: notes, dueDate: dueDate)
+        CoreDataStack.saveContext()
+    }
+    
+    func fetchTasks() {
+        tasks = (try? CoreDataStack.context.fetch(fetchRequest)) ?? []
+    }
+    
+    func update(task: Task, name: String, notes: String?, dueDate: Date?) {
+        task.name = name
+        task.notes = notes
+        task.dueDate = dueDate
+        CoreDataStack.saveContext()
+    }
+    
+    func toggleIsComplete(task: Task) {
+        task.isComplete.toggle()
+        CoreDataStack.saveContext()
+    }
+}
+
