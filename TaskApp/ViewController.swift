dd1f0c5ea2b51d984a942b678a018501e523122b
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-02-28 13:57:20 -0800

    Initial Commit

---
+++ TaskApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskApp
+//
+//  Created by Mehdi MMS on 28/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
